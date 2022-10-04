ad8fa2e67145040396cf0ab535acb90d919a040e
Mazid Ahmad <mazid.ahmad@coinbit.id>
2022-10-04 20:23:28 +0700

    Update core in android and update version

--- android/src/main/kotlin/com/anggaaryas/gojek_courier/Core.kt
+++ android/src/main/kotlin/com/anggaaryas/gojek_courier/Core.kt
@@ -1,6 +1,6 @@
 package com.anggaaryas.gojek_courier
-​
-​
+
+
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
@@ -15,20 +15,20 @@ import io.flutter.plugin.common.EventChannel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import java.util.*
-​
+
 class GojekCourierCore(val receiveSink: EventChannel.EventSink, val logger: Listener, val context: Context) {
-​
+
     private lateinit var mqttClient: MqttClient
     private lateinit var courierService: MessageService
     private var disposable = CompositeDisposable()
     private var streamList = mutableMapOf<String, Disposable>();
-​
+
     private val uiThreadHandler: Handler = Handler(Looper.getMainLooper())
-​
+
     fun init(param : CourierParam){
         param.courierConfiguration?.client?.build(context, logger)?.let {
             mqttClient = it
-​
+
             val courier = Courier(
                 configuration = Courier.Configuration(
                     client = it,
@@ -36,35 +36,35 @@ class GojekCourierCore(val receiveSink: EventChannel.EventSink, val logger: List
                 )
             )
             courierService = courier.create()
-​
+
         }
     }
-​
+
     fun connect(param: MqttConnectOptionParam){
         param.build(context, logger)?.let { mqttClient.connect(it) }
     }
-​
+
     fun disconnect(){
         mqttClient.disconnect()
         for ((key, value) in streamList) {
             value.dispose()
         }
     }
-​
+
     fun subscribe(topic: String, qos: QoS){
         when(qos){
             QoS.ZERO -> courierService.subscribeQosZero(topic = topic)
             QoS.ONE -> courierService.subscribeQosOne(topic = topic)
             QoS.TWO -> courierService.subscribeQosTwo(topic = topic)
         }
-​
+
     }
-​
+
     fun unsubscribe(topic: String){
         courierService.unsubscribe(topic = topic)
         streamList[topic]?.dispose()
     }
-​
+
     fun send(topic: String, message: String, qos: QoS){
         when(qos){
             QoS.ZERO -> courierService.sendQosZero(
@@ -81,7 +81,7 @@ class GojekCourierCore(val receiveSink: EventChannel.EventSink, val logger: List
             )
         }
     }
-​
+
     fun sendByte(topic: String, message: ByteArray, qos: QoS){
         when(qos){
             QoS.ZERO -> courierService.sendByteQosZero(
@@ -98,7 +98,7 @@ class GojekCourierCore(val receiveSink: EventChannel.EventSink, val logger: List
             )
         }
     }
-​
+
     fun listen(topic:String){
         Timber.tag("Courier-Log").d("coba listen $topic...")
         if(!streamList.containsKey(topic)){
@@ -116,7 +116,7 @@ class GojekCourierCore(val receiveSink: EventChannel.EventSink, val logger: List
             uiThreadHandler.post{
                 receiveSink.success("{\"topic\" : \"$topic\", \"data\": ${it.contentToString()}}")
             }
-​
+
             Timber.tag("Courier-Log").d("${it.contentToString()}")
         })*/
     }
